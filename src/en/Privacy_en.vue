ba6e0262527e110adcbd0ea712f0a76507f33376
zhangjh451 <zhangjh451@midea.com>
2023-12-25 17:46:54 +0800

    update

--- src/en/Privacy_en.vue
+++ src/en/Privacy_en.vue
@@ -9,10 +9,10 @@
             <p>During the use of SmartSearch App, only the necessary album permissions are relied upon. The App and the developer will not collect any user data.</p>
             <p>Local model calculations do not rely on network services, please feel free to use.</p>
 
-            <p>Necessary application authorizations:</p>
-            <p>1. Network is required for the first run: Download the AI text and image models needed for operation. After downloading, network permission is no longer needed.</p>
-            <p>2. Storage read and write permissions: Need to scan photos to build index and delete photos when preview photos.</p>
-            <p>3. Notification sending permissions: Image index building task is time-consuming, need to send notifications when build task running on the background.</p>
+            <p>Necessary application permissions:</p>
+            <p>1. Storage read and write permissions: Need to scan photos to build index and delete photos when preview photos.</p>
+            <p>Optional application permissions:</p>
+            <p>2. Notification sending permissions: Image index building task is time-consuming, need to send notifications when build task running on the background.</p>
             <p>FeedBack & Contact:</p>
             <p>Problems feedbacking or want to contact, email to: zhangjh_initial@126.com</p>
         </div>
